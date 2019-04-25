899525b6529d0c0e5a36e3d7e42aabde08586dfc
samhamra <samhamrasnya@gmail.com>
2019-04-25 16:00:09 +0200

    fix cors

--- server/server.js
+++ server/server.js
@@ -60,11 +60,6 @@ app.use(session({
     maxAge: TWO_HOURS
   }
 }))
-app.use(cors({credentials: true, origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    }
-  }}));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(passport.initialize());
@@ -76,6 +71,12 @@ fs.readFile('data/forum.json', (err, data) => {
   if (err) throw err;
   forum = JSON.parse(data);
 });
+
+app.options('*', cors({credentials: true, origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    }
+  }}))
 
 app.post('/register', function(req, res) {
   if(users.some(user=> user.username === req.body.username)) {
